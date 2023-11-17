55d29ef1e1087a5b1f42b582999e157908b65f6b
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-11-17 19:31:23 +0500

    description done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import Desc from './components/Description/Desc';
 import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="">
       <Hero />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className="text-3xl font-bold underline">
-          
-      Hello world!
-    </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Desc />
     </div>
   );
 }
