73c1cce2eb5b923bb5bc7e1ab5eccceb0d71a3a1
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-11-19 05:11:10 +0500

    completed

--- src/components/Features/Feature.jsx
+++ src/components/Features/Feature.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Feature() {
   return (
-    <div className="w-full ">
+    <div className="w-full">
       <div className="container mx-auto p-5 lg:p-0">
         <div className="hidden lg:flex flex-col gap-5 relative ">
           <div className="flex flex-col text-center items-center">
@@ -10,8 +10,10 @@ export default function Feature() {
             <h1 className="text-6xl font-bold text-[#5BBB3D]">IN 5 STEPS</h1>
           </div>
           <div className="flex gap-1 w-full">
-            <div className="flex flex-col items-center gap-2  absolute lg:top-[149%] top-[185%] 2xl:left-[7%] xl:left-[4%] 
-            lg:left-[-1%] md:left-[-13%] sm:left-[-20%]">
+            <div
+              className="flex flex-col items-center gap-2  absolute lg:top-[149%] top-[185%] 2xl:left-[8%] xl:left-[4%] 
+            lg:left-[-1%] md:left-[-13%] sm:left-[-20%]"
+            >
               <div className="text-xl font-bold text-[#25C5D9]">Spy</div>
               <p className="lg:text-sm text-xs w-3/12 text-right lg:w-6/12 lg:mr-[44%] mr-[18%]">
                 First we use insider information to find outwhat supplement
@@ -31,7 +33,7 @@ export default function Feature() {
               </p>
             </div>
 
-            <div className="flex flex-col items-center gap-2 absolute top-[370%] 2xl:left-[-2%] xl:left-[-7%] lg:left-[-10%]">
+            <div className="flex flex-col items-center gap-2 absolute top-[370%] 2xl:left-[4%] xl:left-[-7%] lg:left-[-10%]">
               <div className="text-xl font-bold text-[#FFAC1E] ">
                 Funnel hack
               </div>
@@ -60,55 +62,53 @@ export default function Feature() {
         </div>
         {/* /// mobile view /// */}
         <div className="lg:hidden flex flex-col gap-5">
-        <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center">
             <p className="text-lg font-bold">How to become successful </p>
             <h1 className="text-6xl font-bold text-[#5BBB3D]">IN 5 STEPS</h1>
           </div>
-         <div>
-         <img src="/futureimg/future.png" alt="" />
-         </div>
-         <div className="flex gap-2 pt-5">
-              <div className="text-xl font-bold text-[#25C5D9]">Spy</div>
-              <p className="text-sm text-left">
-                First we use insider information to find outwhat supplement
-                funnels are hot.
-              </p>
-            </div>
-            <div className="flex gap-2">
+          <div>
+            <img src="/futureimg/future.png" alt="" />
+          </div>
+          <div className="flex gap-2 pt-5">
+            <div className="text-xl font-bold text-[#25C5D9]">Spy</div>
+            <p className="text-sm text-left">
+              First we use insider information to find outwhat supplement
+              funnels are hot.
+            </p>
+          </div>
+          <div className="flex gap-2">
             <div className="text-lg font-bold text-[#920F3B]">
-                Rinse & repeat
-              </div>             
-               <p className="text-sm text-left">
+              Rinse & repeat
+            </div>
+            <p className="text-sm text-left">
               Then we rinse and repeat! You can do this again and again and
-                build as many NutraFunnels as you want! it's just that SIMPLE!
-              </p>
-            </div>
-            <div className="flex gap-2">
-            <div className="text-xl font-bold text-[#FFAC1E] ">
-                Funnel hack
-              </div>           
-               <p className="text-sm text-left">
-               Then we rinse and repeat! You can do this again and again and
-                build as many NutraFunnels as you want! it's just that SIMPLE!
-              </p>
-            </div>
-            <div className="flex gap-2">
+              build as many NutraFunnels as you want! it's just that SIMPLE!
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <div className="text-xl font-bold text-[#FFAC1E] ">Funnel hack</div>
+            <p className="text-sm text-left">
+              Then we rinse and repeat! You can do this again and again and
+              build as many NutraFunnels as you want! it's just that SIMPLE!
+            </p>
+          </div>
+          <div className="flex gap-2">
             <div className="text-xl font-bold text-[#E64857]">
-                Traffic outsource
-              </div>        
-               <p className="text-sm text-left">
-               Then we outsource all of the traffic-running to an affiliate
-                network and PROFIT!
-              </p>
+              Traffic outsource
             </div>
-            <div className="flex gap-2">
+            <p className="text-sm text-left">
+              Then we outsource all of the traffic-running to an affiliate
+              network and PROFIT!
+            </p>
+          </div>
+          <div className="flex gap-2">
             <div className="text-xl font-bold text-[#25C5D9]">Outsource</div>
-      
-               <p className="text-sm text-left">
-               We find an offshore outsourcer to put the whole funnel together
-                for us.
-              </p>
-            </div>
+
+            <p className="text-sm text-left">
+              We find an offshore outsourcer to put the whole funnel together
+              for us.
+            </p>
+          </div>
         </div>
       </div>
     </div>
